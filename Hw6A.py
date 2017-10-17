bf8c77f737de050d1b87ee5d093e32df108d6a3f
Adam Benson <adambens@umich.edu>
2017-10-16 22:53:46 -0400

    Homework 6 parts A and B

--- Hw6A.py
+++ Hw6A.py
@@ -23,20 +23,3 @@ print("Count:", len(num_count))
 print("Sum:", sum(num_count))
 
 
-"""
-ctx = ssl.create_default_context()
-ctx.check_hostname = False
-ctx.verify_mode = ssl.CERT_NONE
-
-url = input('Enter - ')
-html_doc = urlopen(url, context=ctx).read()
-
-count = []
-soup = BeautifulSoup(html_doc, 'html.parser')
-for tag in soup.find_all('span'):
-	tag = str(tag)
-	x = tag.split('>')[1]
-	y = x.split('<')
-	count.append(int(y[0]))
-print('Sum: ', sum(count))
-"""
